9d55b5f5aacfa47f54a2b1e10c1bb36f0114df69
Race <race@alumni.unav.es>
2024-09-10 18:25:29 +0200

    prueba2

--- prueba.py
+++ prueba.py
@@ -1,4 +1,16 @@
 import cantools
+import re
 db = cantools.db.load_file("./TeR_DATABASES/TER.dbc")
 
-print(db.messages)
+
+PATTERN = r"(can0\s+)(\d+)(\s+\[8\]\s+)([A-Fa-f0-9\s]+)"
+
+# cargamos el archivo
+
+log = open("./RUN0.log")
+for line in log:
+    msgGroups = re.match(PATTERN,line)
+    if msgGroups:
+        print(msgGroups[1])
+
+
